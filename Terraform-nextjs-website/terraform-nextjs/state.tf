4edbb63a3960c60cfe735a9fd47d4ab309789f0f
Nilaj0914 <nilaj.majumder0814@gmail.com>
2025-09-22 18:05:07 +0530

    fixed submodules to regular files

--- Terraform-nextjs-website/terraform-nextjs/state.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "nm-travelease-terraformstate"
-    key = "global/s3/terraform.tfstate"
-    region = "ap-south-1"
-    dynamodb_table = "travelease-tfstate-table"
-  }
-}
